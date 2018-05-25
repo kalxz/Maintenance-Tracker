c22d28688ea403e883e26d3663b84e370723d4fe
David <kalxz68@gmail.com>
2018-05-25 16:38:33 +0100

    File to store request

---
+++ Server/request.js
@@ -0,0 +1,19 @@
+const requests = [
+    {
+        id: 1,
+        item: "Printer",
+        requestType: "repair"
+    },
+
+    {
+        id: 2,
+        item: "Laptop",
+        requestType: "repair"
+    },
+    {
+        id: 3,
+        item: "Air condition",
+        requestType: "maintenance"
+    }
+];
+module.exports = requests;
