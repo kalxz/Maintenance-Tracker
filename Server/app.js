437201cc81ad60315dda96837e0d24d6ec9795d3
David <kalxz68@gmail.com>
2018-05-25 05:13:02 +0100

    Entry point of API

---
+++ Server/app.js
@@ -0,0 +1,31 @@
+var express = require("express");
+const app = express();
+const jsonParser = require("body-parser").json;
+
+app.use(jsonParser());
+
+const routes = require("../server/routes");
+
+app.use("/api/v1/users/requests", routes);
+
+//Catch error and forward to error handler
+app.use((req,res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+//Error Handler
+app.use((err, req, res, next) => {
+	res.status(err.status || 500);
+	res.json({
+		error: {
+			message: err.message
+		}
+	})
+});
+
+
+app.listen(3000, () => {
+ console.log("The app is listening on port 3000")
+});
